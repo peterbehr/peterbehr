07b88319e07e07dfbb2c3e087126583e57170eb5
Peter Behr <pbehr@uchicago.edu>
2010-11-15 18:59:23 -0800

    Added some text.

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
                 <div id="sider">
                 </div>
                 <div id="content">
+                    <p>
+                        I am a frontend developer. I speak English, Polish, Chinese, JavaScript, HTML, CSS, and Python...and a little French. I love bikes, Bill Gibson's writing, good produce, air travel, and living in San Francisco, definitely one of the most beautiful cities in the world.
+                    </p>
                 </div>
 
         <?php
