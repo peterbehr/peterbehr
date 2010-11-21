1fad8c38ce4aa6e605a154050a119a96af863a8b
Peter Behr <pbehr@uchicago.edu>
2010-11-20 20:44:43 -0800

    Tearing stuff apart, changing venue.

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
-            </div><!-- #body -->
-            <div id="footer">
-                <span class="normal">Cartograph</span> is being made in California by yours truly in 2010, and uses <a href="http://wordpress.com">WordPress</a> to do the dirty work.
-            </div>
-        </div><!-- #container -->
-    </div><!-- #border -->
+        </div><!-- #body -->
+        <div id="footer">
+            <span class="normal">Cartograph</span> is being made in California by yours truly in 2010, and uses <a href="http://wordpress.com">WordPress</a> to do the dirty work.
+        </div>
+    </div><!-- #container -->
 </div><!-- #background -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -19,51 +19,52 @@
 <body <?php body_class(); ?>>
 
 <div id="background">
-    <div id="border">
-        <div id="container">
-            <div id="header">
-                <div id="nav">
-                    <ul id="sections">
-                        <li class="normal caps">
-                            <a href="<?php echo(get_bloginfo('url')); ?>">
-                                Peter Behr
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?php echo(get_bloginfo('url') . '/projects/'); ?>">
-                                Projects
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?php echo(get_bloginfo('url') . '/resume/'); ?>">
-                                Resume
-                            </a>
-                        </li>
-                        <li class="end">
-                            <a href="<?php echo(get_bloginfo('url') . '/contact/'); ?>">
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
-                    <ul id="scale" class="clear">
-                        <li>
-                            <span>10</span>
-                        </li>
-                        <li>
-                            <span>20</span>
-                        </li>
-                        <li>
-                            <span>30</span>
-                        </li>
-                        <li>
-                            <span>40</span>
-                        </li>
-                        <li class="end">
-                            <span>50</span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="clear zero">
-                </div>
+    <div id="container">
+        <div id="header">
+            <div id="map">
             </div>
-            <div id="body">
+            <div id="nav">
+                <ul id="sections">
+                    <li class="normal caps">
+                        <a href="<?php echo(get_bloginfo('url')); ?>">
+                            Peter Behr
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo(get_bloginfo('url') . '/projects/'); ?>">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo(get_bloginfo('url') . '/resume/'); ?>">
+                            Resume
+                        </a>
+                    </li>
+                    <li class="end">
+                        <a href="<?php echo(get_bloginfo('url') . '/contact/'); ?>">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+                <ul id="scale" class="clear">
+                    <li>
+                        <span>10</span>
+                    </li>
+                    <li>
+                        <span>20</span>
+                    </li>
+                    <li>
+                        <span>30</span>
+                    </li>
+                    <li>
+                        <span>40</span>
+                    </li>
+                    <li class="end">
+                        <span>50</span>
+                    </li>
+                </ul>
+            </div>
+            <div id="shadow">
+            </div>
+        </div>
+        <div id="body">
