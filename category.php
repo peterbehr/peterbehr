ef59f2e29e7e045962548e0e4cc19ad95d1c38ab
Peter Behr <pbehr@uchicago.edu>
2010-11-20 19:57:56 -0800

    Trying a change in the category page structure. Might not work.

--- category.php
+++ category.php
@@ -1,34 +1,64 @@
-<?php
-/**
- * The template for displaying Category Archive pages.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<?php (is_category('Projects')) { ?>
 
-		<div id="container">
-			<div id="content" role="main">
+                <!--div id="sider">
+                </div-->
+                <div id="content">
+                    <p>
+                        Recentlyish:
+                    </p>
+                    <ul>
+                        <li>
+                            <a href="http://pbehr.com/css_zen_garden/zurich/">Zurich</a> (theme for the <a href="http://csszengarden.com">CSS Zen Garden</a>)
+                        </li>
+                        <li>
+                            <a href="http://about.me">About.me</a> (I made some of this)
+                        </li>
+                        <li>
+                            <a href="http://pbehr.com/">Cartograph</a> (this site, in progress)
+                        </li>
+                        <li>
+                            <a href="http://oliviacheng.net">Olivia Cheng</a> (a splash page)
+                        </li>
+                        <li>
+                            <a href="http://refractor.me">Refractor</a> (in progress)
+                        </li>
+                        <li>
+                            <a href="http://pbehr.com/colorsort/">Colorsort</a> (jQuery experiment)
+                        </li>
+                    </ul>
+                    <p>
+                        Also:
+                    </p>
+                    <ul>
+                        <li>
+                            <a href="http://flickr.com/photos/peterbehr/">Flickr</a> (photo portfolio)
+                        </li>
+                        <li>
+                            <a href="http://aristarchus.deviantart.com/gallery/">DeviantArt</a> (temporary art portfolio)
+                        </li>
+                    </ul>
+                </div>
 
-				<h1 class="page-title"><?php
-					printf( __( 'Category Archives: %s', 'twentyten' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-				?></h1>
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $category_description . '</div>';
+<?php } else if (is_category('Contact')) { ?>
 
-				/* Run the loop for the category page to output the posts.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-category.php and that will be used instead.
-				 */
-				get_template_part( 'loop', 'category' );
-				?>
+                <!--div id="sider">
+                </div-->
+                <div id="content">
+                    <ul>
+                        <li>
+                            <a href="http://www.linkedin.com/in/peterbehr">LinkedIn</a> (hire me!)
+                        </li>
+                        <li>
+                            <a href="http://twitter.com/peterbehr/">Twitter</a>
+                        </li>
+                        <li>
+                            <a href="http://facebook.com/peterbehr/">Facebook</a>
+                        </li>
+                    </ul>
+                </div>
 
-			</div><!-- #content -->
-		</div><!-- #container -->
+<?php } ?>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
