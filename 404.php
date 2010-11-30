9d23a986ac0ecd966bcacfc10110663d589a876b
Peter Behr <pbehr@uchicago.edu>
2010-11-29 17:06:56 -0800

    Once more...

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <p>
-    Oh noes! The thing you wanted to see is not here...<a href="/contact/">let me no, eh?
+    Oh noes! The thing you wanted to see is not here...<a href="/contact/">let me know</a>, eh?
 </p>
 
 <?php get_footer(); ?>
