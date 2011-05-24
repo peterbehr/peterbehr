c7396c2da5e513d76c99af9874f123f74789488f
Peter Behr <pbehr@uchicago.edu>
2011-05-23 19:57:39 -0700

    Changed anchor color (temporarily), updated footer, etc.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div id="footer">
     <div class="grid_container centered">
         <p class="grid_3_3 italic">
-            Made in 2011 with loving care &amp; worry around Berkeley, California. Biggest ups go to <a href="http://webfaction.com">WebFaction</a>, <a href="http://apple.com">Apple Computer</a>, and <a href="http://wordpress.com">WordPress</a>. A tediously comprehensive colophon will come later. <a href="#">Now back up!</a>
+            Made in 2011 with loving care &amp; worry by Peter Behr in and around Berkeley, California. A heavily abridged list of your options at this point: <a href="<?php bloginfo('url'); ?>/meta/">read the colophon</a>, <a href="<?php bloginfo('url'); ?>">go home</a>, or <a href="#">return to the top</a>.
         </p>
     </div>
 </div>
